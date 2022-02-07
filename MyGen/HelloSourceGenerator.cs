1fd516370b39ed09eeae651c68f600f462a31f52
Youssef1313 <youssefvictor00@gmail.com>
2022-02-07 10:36:52 +0200

    Initial commit

---
+++ MyGen/HelloSourceGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using MS.CA.Utilities.CSharp.Generators;
+
+namespace MyGen
+{
+    [Generator]
+    public class HelloSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // Use a type from the package.
+            var writer = new CSharpGeneratorWriter();
+            writer.Builder.AppendLine("#error The generator was run successfully :)");
+            
+            context.AddSource("File.g.cs", writer.Builder.ToString());
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // No initialization required for this one
+        }
+    }
+}
